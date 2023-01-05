a8afc3ef1413c3cbc9087eea2fceccd2c17333a2
Sam Hedges <sam.hedges.sh@gmail.com>
2023-01-05 16:08:22 +0000

    Started Anim Script

--- Assets/Project/Runtime/_Scripts/Animation/ProceduralAnimation.cs
+++ Assets/Project/Runtime/_Scripts/Animation/ProceduralAnimation.cs
@@ -11,7 +11,7 @@ public class ProceduralAnimation : MonoBehaviour
     [SerializeField] private AnimationCurve stepCurve;
     [SerializeField] private float stepDuration = 0.5f;
 
-    [Serializable]
+    [Serializable] 
     private class Foot
     {
         public Transform footTransform;
@@ -20,10 +20,34 @@ public class ProceduralAnimation : MonoBehaviour
         internal Vector3 PreviousPosition;
         internal Vector3 TargetPosition => targetTransform.position;
         internal float stepProgress = 0f;
+        internal bool isStepping = false;
+        
+        internal void UpdatePreviousPosition()
+        {
+            PreviousPosition = Position;
+        }
     }
 
-    [SerializeField] private Foot[] feet;
+    [SerializeField] private Transform rootTransform;
+    private Vector3 worldUp;
+    
+    // NonReorderable is a custom attribute to prevent reordering in the inspector
+    // It's used in this case to prevent a nested-class array, overlapping-inspector bug
+    [SerializeField] [NonReorderable] private Foot[] feet;
 
+    
+    
+
+    private float raycastRange = 1f;
+    private Vector3 lastBodyUp;
+    private bool[] legMoving;
+    private int nbLegs;
+    
+    private Vector3 velocity;
+    private Vector3 lastVelocity;
+    private Vector3 lastBodyPos;
+
+    private float velocityMultiplier = 15f;
 
     private void Awake()
     {
@@ -37,8 +61,15 @@ public class ProceduralAnimation : MonoBehaviour
 
     private void FixedUpdate()
     {
+        DriveLegs();
+    }
+    
+    private void DriveLegs()
+    {
         foreach (var foot in feet)
         {
+            if (foot.isStepping) continue;
+            
             // Introduced Variables to reduce the amount of calls to the transform
             Vector3 targetPosition = foot.TargetPosition;
             Vector3 footPosition = foot.Position;
@@ -48,9 +79,11 @@ public class ProceduralAnimation : MonoBehaviour
 
             if (distance > stepDistance)
             {
-                foot.PreviousPosition = targetPosition;
-                foot.footTransform.position = Vector3.Lerp(footPosition, targetPosition, stepCurve.Evaluate(foot.stepProgress));
-                foot.stepProgress += Time.deltaTime / stepDuration;
+                // foot.PreviousPosition = targetPosition;
+                // foot.footTransform.position = Vector3.Lerp(footPosition, targetPosition, stepCurve.Evaluate(foot.stepProgress));
+                // foot.stepProgress += Time.deltaTime / stepDuration;
+
+                StartCoroutine(PerformStep(foot, targetPosition));
             } else
             {
                 foot.footTransform.position = foot.PreviousPosition;
@@ -58,7 +91,31 @@ public class ProceduralAnimation : MonoBehaviour
             }
         }
     }
+    
+    private IEnumerator PerformStep(Foot foot, Vector3 targetPosition)
+    {
+        foot.isStepping = true;
+        foot.stepProgress = 0f;
+        
+        Vector3 startPos = foot.Position; // Set the starting postition to the legs current postition
 
+        while (foot.stepProgress < 1f)
+        {
+            foot.stepProgress += Time.deltaTime / stepDuration;
+            float height = stepCurve.Evaluate(foot.stepProgress) * stepHeight;
+            Vector3 newPos = Vector3.Lerp(startPos, targetPosition, foot.stepProgress);
+            foot.footTransform.position = new Vector3(newPos.x, height, newPos.z);
+            yield return new WaitForFixedUpdate(); // Waits until next physics step to continue
+        }
+        
+        foot.footTransform.position = targetPosition;
+        foot.PreviousPosition = targetPosition;
+        foot.stepProgress = 0f;
+        foot.isStepping = false;
+    }
+    
+    
+    
     // Fix Leg to the ground
     
     
@@ -89,10 +146,15 @@ public class ProceduralAnimation : MonoBehaviour
             // Draw the target position and it's size
             Gizmos.color = Color.white;
             Gizmos.DrawWireSphere(targetPosition, stepDistance);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(footPosition, footPosition + Vector3.up * stepHeight);
             
+            #region LineBetweenFootAndTarget
+
             // Calculate the distance between foot and target
             float distance = Vector3.Distance(footPosition, targetPosition);
-            
+
             // Colour the distance between foot and target green if it's less than stepDistance
             if (distance > stepDistance)
             {
@@ -105,6 +167,8 @@ public class ProceduralAnimation : MonoBehaviour
             
             // Draw the distance between foot and target
             Gizmos.DrawLine(footPosition, targetPosition);
+            
+            #endregion
         }
     }
     
